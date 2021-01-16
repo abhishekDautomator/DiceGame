ba34c1e96a703298dc6a7495b208706d05168d01
Abhishek <narayan.abhishek.28@gmailcom>
2021-01-16 14:01:48 +0530

    taking user name input and showing the values

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var player1name=prompt("Enter player 1 name");
+var player2name=prompt("Enter player 2 name");
+
 function generateNumberFrom1To6(){
     return Math.ceil(Math.random()*6);
 }
@@ -9,18 +12,20 @@ var randomDiceImg1 ="images/dice" + num1+ ".png";
 
 var randomDiceImg2 ="images/dice" + num2+ ".png";
 
+document.querySelector(".playerTxt1").innerHTML=player1name;
+document.querySelector(".playerTxt2").innerHTML=player2name;
 document.querySelector("#dice1").setAttribute("src",randomDiceImg1);
 document.querySelector("#dice2").setAttribute("src",randomDiceImg2);
 
-console.log("Player 1 has got "+num1);
-console.log("Player 2 has got "+num2);
+console.log(player1name+" has got "+num1);
+console.log(player2name+" has got "+num2);
 
 if(num1>num2){
-  document.querySelector("h1").innerHTML="🚩 Player 1 Wins!";
+  document.querySelector("h1").innerHTML="🚩 Hurray "+player1name+" won!";
   document.querySelector(".invisible").style.visibility='visible';
 }
 else if(num1<num2){
-  document.querySelector("h1").innerHTML="🚩 Player 2 Wins!";
+  document.querySelector("h1").innerHTML="🚩 Hurray "+player2name+" won!";
   document.querySelector(".invisible").style.visibility='visible';
 }
 else{
